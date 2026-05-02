b405ed651e168affc963415acf51827848a735aa
Luke Lummis <luke.lummis@hotmail.com>
2026-05-02 23:56:38 +0800

    Fixed some stuff

--- code/particle_swarm_algorithm.py
+++ code/particle_swarm_algorithm.py
@@ -21,7 +21,7 @@ DEBUG = 0
 
 
 class PSOConfig:
-    def __init__(self, size=20, pw=0.5, gw=0.5, w=0.5, v_max=4.0, stag_lim=10):
+    def __init__(self, size=20, pw=0.5, gw=0.5, w=0.5, v_max=4.0, stag_lim=10, max_iter=500):
         self.size = size
         self.pw = pw  # Personal best weight
         self.gw = gw  # Group best weight
@@ -29,6 +29,7 @@ class PSOConfig:
         self.v_max = v_max  # Max absolute value of velocity vector i.e. -v_max < v < v_max
         self.stag_lim = stag_lim  # Number of iterations with no improvement to trigger swarm regeneration
         self.regen_size = self.size // 2  # Number of particles to regenerate
+        self.max_iterations = max_iter
 
 
 class Particle:
@@ -36,6 +37,7 @@ class Particle:
         self.solution = []
         self.vel = []
         self.p_best = None
+        self.p_best_cost = float('-inf')
 
 
 def pso(cfg=None):
@@ -52,6 +54,9 @@ def pso(cfg=None):
     iteration = 0
     regens = 0
     stagnation = 0
+    max_iteration = cfg.max_iterations
+
+    gen_data_list = []
 
     # Swarm initialisation
     swarm_list = create_swarm(cfg.size)
@@ -68,7 +73,7 @@ def pso(cfg=None):
         print(f"|DEBUG| g_best_idx = {g_best_idx}\n")
 
     # Start of main PSO loop
-    while iteration <= 500 or max(cost_list) >= 0:
+    while max(cost_list) < 0 and iteration < max_iteration:
         # Regenerate part of swarm if stagnation detected
         if stagnation >= stag_lim:
             if DEBUG:
@@ -100,7 +105,7 @@ def pso(cfg=None):
                 if DEBUG:
                     print(
                         f"|DEBUG| new_pos[{vect_idx}] = {p.solution[vect_idx]} + {p.vel[vect_idx]} = {p.solution[vect_idx] + p.vel[vect_idx]}")
-                new_pos = (p.solution[vect_idx] + p.vel[vect_idx]) % 5
+                new_pos = round((p.solution[vect_idx] + p.vel[vect_idx])) % len(cf.employees)
                 p.solution[vect_idx] = new_pos
 
             if DEBUG:
@@ -108,8 +113,8 @@ def pso(cfg=None):
                 print(f"|DEBUG| |After| p.vel: {p.vel}\n")
 
         # Calculate new swarm fitness and track stagnation
-        new_fitness_list = swarm_cost(swarm_list)
-        if max(new_fitness_list) <= max(cost_list):
+        new_cost_list = swarm_cost(swarm_list)
+        if max(new_cost_list) <= max(cost_list):
             stagnation += 1
         else:
             stagnation = 0
@@ -117,16 +122,17 @@ def pso(cfg=None):
         if DEBUG:
             if DEBUG: print(f"|DEBUG| Old cost_list: {cost_list}  max = {max(cost_list)}")
             if DEBUG: print(
-                f"|DEBUG| New cost_list: {new_fitness_list}  max = {max(new_fitness_list)}")
+                f"|DEBUG| New cost_list: {new_cost_list}  max = {max(new_cost_list)}")
             if DEBUG: print(f"|DEBUG| stagnation = {stagnation}")
 
         # Update 'p_best' if new p.solution better than p.p_best
         for p_idx, p in enumerate(swarm_list):
-            if new_fitness_list[p_idx] > round(-cf.TotalCostOfSolution(p.p_best), 3):
+            if new_cost_list[p_idx] > p.p_best_cost:
                 p.p_best = p.solution.copy()
+                p.p_best_cost = new_cost_list[p_idx]
 
         # Update fitness list and g_best_idx/pos
-        cost_list = new_fitness_list
+        cost_list = new_cost_list
         g_best_idx = cost_list.index(max(cost_list))
         g_best_pos = swarm_list[g_best_idx].solution.copy()
 
@@ -137,7 +143,14 @@ def pso(cfg=None):
             print(f"|DEBUG|  - cost_list: {cost_list}")
             input("")
 
-    return g_best_pos, iteration, regens
+        average_cost = sum(cost_list) / len(cost_list)
+        gen_data = cf.GenerationData(min(cost_list), average_cost, max(cost_list), cf.TotalCostOfSolution(g_best_pos))
+        gen_data_list.append(gen_data)
+
+    print(
+        f"Found solution: {solution} cost = {cost}, after {iterations} iterations ({regenerations} regenerations)")
+
+    return gen_data_list
 
 
 def create_swarm(swarm_size: int) -> list[Particle]:
@@ -151,7 +164,7 @@ def create_swarm(swarm_size: int) -> list[Particle]:
 def create_particle() -> Particle:
     particle = Particle()
     particle.solution = cf.GenerateRandomSolution()
-    particle .vel = cf.GenerateRandomSolution()
+    particle.vel = cf.GenerateRandomSolution()
     particle.p_best = particle.solution[:]
 
     return particle
@@ -201,16 +214,6 @@ def regen_swarm(swarm_list: list[Particle], fitness_list: list[int], cfg: PSOCon
         print(f"|DEBUG| new fitness_list: {fitness_list}")
 
 
-def is_diagonal(row1, col1, row2, col2):
-    if row1 == row2 and col1 == col2:  # Don't count same position as diagonal
-        return False
-
-    if abs(row1 - row2) == abs(col1 - col2):
-        return True  # Return True if 'pos1' diagonal to 'pos2'
-    else:
-        return False
-
-
 def print_swarm(swarm_list: list):
     print("|DEBUG| Swarm list:")
     for p in swarm_list:
@@ -219,30 +222,7 @@ def print_swarm(swarm_list: list):
     print("")
 
 
-def display_board(queen_list: list):
-    board = []
-    for i in range(8):
-        board.append([0, 0, 0, 0, 0, 0, 0, 0])
-
-    for row, col in enumerate(queen_list):
-        board[7 - row][col - 1] = 'X'
-
-    board_str = ""
-    for l in board:
-        board_str += "|DEBUG| "
-        for e in l:
-            if e == 'X':
-                board_str += f"{GREEN}{e}{RESET}  "
-            else:
-                board_str += f"{e}  "
-        board_str += '\n'
-
-    print(board_str, end="")
-
-
 if __name__ == "__main__":
     DEBUG = 0
-    pso_cfg = PSOConfig(size=3, pw=0.5, gw=0.5, w=0.5)
-    solution, iterations, regenerations = pso(pso_cfg)
-    print(
-        f"Found solution: {solution} after {iterations} iterations ({regenerations} regenerations)")
+    pso_cfg = PSOConfig(size=20, pw=0.5, gw=0.5, w=0.5, max_iter=100000)
+    solution, cost, iterations, regenerations = pso(pso_cfg)
