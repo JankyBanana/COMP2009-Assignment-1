c29a529a49d9ce8044868a422ff32ca4253e971a
Powerblade <oliver_lunn@yahoo.com>
2026-05-02 12:22:23 +0800

    Added Comments to genetic and ant colony algorithms

--- code/ant_colony_algorithm.py
+++ code/ant_colony_algorithm.py
@@ -5,19 +5,27 @@ import random
 
 from common_functions import *
 
+# How each ant constructs a solution given the pheromone matrix
 def solutionConstruction(pheromone):
     solution = []
+
+    # For each pheromone for task i
     for row in pheromone:
         totalPheromone = sum(row)
+
+        # If total pheromone is 0, select random employee for task
         if totalPheromone <= 0:
             employee = random.randint(0, len(employees) - 1)
             solution.append(employee)
             continue
 
+        # Roulette wheel for weighted selection of employee given pheromone matrix
+        # Random number between 0 and totalPheromone
         r = random.uniform(0, totalPheromone)
         cumulative = 0
         chosenEmployee = 0
 
+        # Randomly select an employee with weighted probabilities given pheromone
         for employee in range(len(employees)):
             cumulative += row[employee]
             if cumulative >= r:
@@ -29,48 +37,65 @@ def solutionConstruction(pheromone):
 
     return solution
 
-
+# Ant Colony Algorithm with optional arguments for iterations (generations), number of ants per iteration, evaporation rate, and depositConstant
 def AntColonyAlgorithm(generations = 500, numberOfAnts = 30, evaporationRate = 0.2, depositConstant = 0.4):
     pheromone = np.ones((len(tasks), len(employees)))
+    # Max possible pheromone level to prevent severe stagnation from a single solution
     maxPheromone = 10
 
+    # Best solution found so far
     bestSolution = (None, -math.inf)
+    # SolutionFound = True if the best possible solution has been found (fitness / cost = 0)
     solutionFound = False
 
+    # Main loop, iterates through each generation
     for generation in range(generations):
         solutions = []
         fitnesses = []
+
         # Ants choose solution
         for ant in range(numberOfAnts):
+            # Generate solution
             solution = solutionConstruction(pheromone)
+            # Determine fitness, which is the negative of cost (higher fitness = lower cost)
             fitness = -TotalCostOfSolution(solution)
             fitnesses.append(fitness)
             solutions.append(solution)
+
+            # Check if this solution is the best so far
             if fitness > bestSolution[1]:
                 bestSolution = (solution, fitness)
                 print(bestSolution, generation)
+
+                # Check if this solution is the best possible solution of fitness / cost = 0
                 if fitness >= 0:
                     print(bestSolution, generation)
                     solutionFound = True
 
-        # Evaporate
+        # Evaporate pheromone to prevent stagnation and encourage exploration
         for row in range(len(pheromone)):
             for column in range(len(pheromone[row])):
                 pheromone[row][column] *= (1 - evaporationRate)
 
-        # Deposit
+        # Deposit pheromone based on solution fitness, higher fitness deposits more pheromone on their pheromone matrix
         i = 0
+        # Iterate through each ant's solution to deposit pheromone
         for solution in solutions:
+            # Calculate amount to be deposited
             deposit_amount = depositConstant / ((-10*fitnesses[i]) + 1)
 
+            # Iterate through each task-employee assignment and deposit pheromone on ant's solution
             ii = 0
             for employee in solution:
                 pheromone[ii][employee] += deposit_amount
+
+                # Cap maximum phermone limit at maxPheromone
                 if pheromone[ii][employee] > maxPheromone:
                     pheromone[ii][employee] = maxPheromone
                 ii += 1
             i += 1
 
+        # If best solution possible is found, terminate algorithm
         if solutionFound:
             break
 
--- code/genetic_algorithm.py
+++ code/genetic_algorithm.py
@@ -3,59 +3,81 @@ import random
 
 from common_functions import *
 
+# Take a random sample of the population, and select the highest fitness gene from that sample
 def SelectRandomHighFitnessSolution(population):
     sampleSize = 5
     solution = random.sample(population, sampleSize)
     solution = max(solution, key=lambda x: x[1])
-    #solution.sort(key=lambda x: x[1], reverse=True)
     return solution
 
-
+# Crossover function that randomly splits parents into offspring
 def Crossover(parent1, parent2):
+    # Random cut off point where the parent gene gets split
     cutpoint = random.randint(1, len(parent1) - 2)
+
+    # Offspring
     child1 = np.concatenate([parent1[:cutpoint], parent2[cutpoint:]])
     child2 = np.concatenate([parent2[:cutpoint], parent1[cutpoint:]])
 
     return child1, child2
 
-
+# Genetic Algorithm with optional arguments of generations, population size per generation, crossover probability, and mutation probability
 def GeneticAlgorithm(generations = 500, populationSize = 50, crossoverProbability = 0.8, mutationProbability = 0.2):
 
     population = []
+
+    # Set the population to a list of random solutions
     for i in range(populationSize):
+        # Generate random solution
         solution = GenerateRandomSolution()
+        # Fitness is negative of the cost (higher cost = lower fitness_
         fitness = -TotalCostOfSolution(solution)
         population.append([solution, fitness])
+
     bestSolution = population[0]
+    # Solution found is true if a solution with a fitness / cost of exactly 0 is found
     solutionFound = False
 
+    # Main loop for each generation
     for generation in range(generations):
-
-        population.sort(key=lambda x: x[1], reverse=True)
+        # population.sort(key=lambda x: x[1], reverse=True)
         offspring = []
 
+        # Loop to make a new population of offspring
         while len(offspring) < populationSize:
+            # Choose a pair of high fitness parents
             parent1 = SelectRandomHighFitnessSolution(population)[0]
             parent2 = SelectRandomHighFitnessSolution(population)[0]
 
+            # Crossover probability check
             if random.random() < crossoverProbability:
                 child1, child2 = Crossover(parent1, parent2)
                 offspring.append(child1)
+
+                # Check to ensure adding another offspring won't exceed population size
                 if len(offspring) < populationSize:
                     offspring.append(child2)
 
+        # Mutation, go over each chromosome in the new offspring population and have a probability to mutate
         for individual in offspring:
             for index in range(len(individual)):
                 if random.random() < mutationProbability:
+                    # Mutate chromosome to random employee assigned to that task
                     individual[index] = random.randint(0, len(employees) - 1)
 
+        # Set population to the new generation of offspring with calculated fitness
         population = []
         for i in range(populationSize):
             fitness = -TotalCostOfSolution(offspring[i])
+
+            # Check if current solution is better than the best found solution
             if fitness > bestSolution[1]:
                 bestSolution = [offspring[i], fitness, generation]
+
+                # Check if this is the best possible solution (no penalties)
                 if fitness >= 0:
                     solutionFound = True
+
             population.append([offspring[i], fitness])
 
         print("-------------NEW GENERATION-----------------")
